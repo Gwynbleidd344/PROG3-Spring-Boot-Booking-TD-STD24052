f07520eb8e4bbd1359c01852e83333a55d9d7edc
Vaniaina <vanimendes344@gmail.com>
2026-03-11 20:29:14 +0300

    feat add booking GET savedBookings endpoint

--- src/main/java/hei/school/fastapi_to_spring_boot/controller/BookingController.java
+++ src/main/java/hei/school/fastapi_to_spring_boot/controller/BookingController.java
@@ -1,5 +1,19 @@
 package hei.school.fastapi_to_spring_boot.controller;
 
-public class BookingController {
+import java.util.ArrayList;
+import java.util.List;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import hei.school.fastapi_to_spring_boot.model.Booking;
+
+@RestController
+public class BookingController {
+    List<Booking> savedBookings = new ArrayList<>();
+
+    @GetMapping("/bookings")
+    public List<Booking> getSavedBookings() {
+        return savedBookings;
+    }
 }
