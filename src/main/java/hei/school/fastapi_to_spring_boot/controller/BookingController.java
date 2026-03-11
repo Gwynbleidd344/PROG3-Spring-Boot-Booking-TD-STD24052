0132fd34525f1d0e7c2095de6e4b0ec24c035e38
Vaniaina <vanimendes344@gmail.com>
2026-03-11 20:16:02 +0300

    feat add basic project folder structure

---
+++ src/main/java/hei/school/fastapi_to_spring_boot/controller/BookingController.java
@@ -0,0 +1,5 @@
+package hei.school.fastapi_to_spring_boot.controller;
+
+public class BookingController {
+
+}
---
+++ src/main/java/hei/school/fastapi_to_spring_boot/model/Booking.java
@@ -0,0 +1,5 @@
+package hei.school.fastapi_to_spring_boot.model;
+
+public class Booking {
+
+}
