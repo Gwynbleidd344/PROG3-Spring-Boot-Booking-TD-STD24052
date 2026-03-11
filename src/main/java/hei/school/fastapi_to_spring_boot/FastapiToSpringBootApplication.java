36fbf8c9aa5ffe240a925e8c2a9cf5ee9d5997d3
Vaniaina <vanimendes344@gmail.com>
2026-03-11 20:12:36 +0300

    project initialization

---
+++ src/main/java/hei/school/fastapi_to_spring_boot/FastapiToSpringBootApplication.java
@@ -0,0 +1,13 @@
+package hei.school.fastapi_to_spring_boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FastapiToSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FastapiToSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hei/school/fastapi_to_spring_boot/FastapiToSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package hei.school.fastapi_to_spring_boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FastapiToSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
