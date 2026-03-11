182cdf2baf246ce7bb13219916cd389eaa1047b3
Vaniaina <vanimendes344@gmail.com>
2026-03-11 20:22:49 +0300

    feat add booking model

--- src/main/java/hei/school/fastapi_to_spring_boot/model/Booking.java
+++ src/main/java/hei/school/fastapi_to_spring_boot/model/Booking.java
@@ -1,5 +1,21 @@
 package hei.school.fastapi_to_spring_boot.model;
 
-public class Booking {
+import java.time.LocalDate;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class Booking {
+    private String clientName;
+    private String phoneNumber;
+    private String email;
+    private int roomNumber;
+    private String roomDescription;
+    private LocalDate bookingDate;
 }
